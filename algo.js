ea91bb1a35cce906a0d6fbc05cce436efd69cb14
wassimtouati1997 <wassimtouati900@gmail.com>
2023-01-30 00:22:39 +0100

    done

---
+++ algo.js
@@ -0,0 +1,25 @@
+//Algorithm (Procedure): Dot product
+ 
+// Input two arrays v1 and v2 of size n
+// Initialize a variable ps to 0
+// Use a nested loop to calculate the dot product:
+// outer loop: iterate i from 0 to n-1
+// inner loop: iterate j from 0 to n-1
+// multiply v1[i] by v2[j] and add it to ps
+// End the procedure
+// Algorithm (Orthogonal vectors using procedure):
+
+// Input two arrays v1 and v2 of size n
+// Call the dot product procedure to get the dot product value in ps
+// If ps is equal to 0, then print "The vectors are orthogonal."
+// Else, print "The vectors are not orthogonal."
+// End the algorithm
+// Algorithm (Orthogonal vectors using function):
+
+// Input two arrays v1 and v2 of size n
+// Call the dot product function to get the dot product value in ps
+// If ps is equal to 0, then print "The vectors are orthogonal."
+// Else, print "The vectors are not orthogonal."
+// End the algorithm
+
+
